a164411e0140f7b8edea222bf758a825349d1a17
Matheus Martins <matheusms280100@gmail.com>
2023-05-22 22:18:17 -0300

    add questions 2

---
+++ question2.js
@@ -0,0 +1,131 @@
+let a = [
+  {
+    "title": 'Das opções abaixo, qual é a melhor maneira de promover um produto como afiliado?',
+    "options": [
+      "Publicar o link de afiliado nas redes sociais e ir jogar videogame",
+      "Enviar 10 emails por dia para a lista de leads",
+      "Criar conteúdo de qualidade para seu público-alvo",
+    ],
+    "right": 2
+  },
+  {
+    "title": 'O que significa o termo "E-book"?',
+    "options": [
+      "Livro eletrônico.",
+      "Curso online.",
+      "Software de gestão de e-mail.",
+    ],
+    "right": 0
+  },
+  {
+    "title": 'O que é copywriting?',
+    "options": ["  Copiar o texto dos concorrentes ",
+      "Técnicas de escrita persuasiva para vendas",
+      "Um modelo de negócios com faturamento por comissões ",
+    ],
+    "right": 1
+  },
+  {
+    "title": 'O que significa “conversão” no mercado digital?',
+    "options": ["  O número de pessoas que realiza uma compra ",
+      "O número de pessoas que clica em um link, mas não compra",
+      "O número de pessoas que se cadastra em uma lista de e-mails ",
+    ],
+    "right": 0
+  },
+  {
+    "title": 'O que é o lançamento de um infoproduto?',
+    "options": ["  O processo de divulgação oficial de um produto ",
+      "O marketing de um produto que ainda não tem data para ficar pronto",
+      "A atualização de um infoproduto que já existe ",
+    ],
+    "right": 0
+  },
+  {
+    "title": 'O que significa a expressão abrir carrinho?',
+    "options": ["  Trocar de carro com a grana do digital",
+      "Chamar um afiliado para vender o seu infoproduto",
+      "Colocar um infoproduto à venda",
+    ],
+    "right": 2
+  },
+  {
+    "title": 'O que é o ROI?',
+    "options": ["  Sigla para “Custo por Aquisição”",
+      "Sigla para “Receita por Usuário”",
+      "Sigla para “Retorno Sobre Investimento”",
+    ],
+    "right": 2
+  },
+  {
+    "title": 'O que é o pixel de conversão?',
+    "options": ["  Um código para rastrear conversões em um site",
+      "Uma imagem para chamar a atenção dos visitantes",
+      "Um sistema de análise de dados para afiliados ",
+    ],
+    "right": 0
+  },
+  {
+    "title": 'O que é isca digital?',
+    "options": ["  Um produto barato para incentivar a compra",
+      "Um produto gratuito para gerar uma lista de leads",
+      "Um produto premium para incentivar a compra ",
+    ],
+    "right": 1
+  },
+
+  {
+    "title": 'O que é remarketing?',
+    "options": ["  Exibir anúncios para quem já visitou uma página ",
+      "Otimizar as conversões de uma landing pages",
+      "Modelo de negócios com faturamento por comissão",
+    ],
+    "right": 0
+  },
+
+  {
+    "title": 'O que é o Google Ads?',
+    "options": ["  Otimizar um site para atrair mais cliques orgânicos",
+      "Pagar diretamente ao Google para promover links",
+      "Sistema de publicidade online do Google",
+    ],
+    "right": 2
+  },
+
+  {
+    "title": 'O que é o Google Analytics?',
+    "options": ["  Sistema de publicidade online do Google",
+      "Ferramentas para analisar o tráfego de um site",
+      "Sistema de comissão por vendas feitas pelo Google",
+    ],
+    "right": 1
+  },
+
+  {
+    "title": 'O que é o programa de afiliados?',
+    "options": ["  É quando um infoprodutor recruta afiliados para seus produtos",
+      "Publicidade online para quem já visitou o site do afiliado",
+      "Técnicas para gerenciar listas de e-mails de afiliados",
+    ],
+    "right": 0
+  },
+
+  {
+    "title": 'Qual a principal função da página de vendas?',
+    "options": ["  Apresentar informações sobre um afiliado ",
+      "Convencer o visitante a comprar",
+      "Apresentar informações sobre um negócio digital ",
+    ],
+    "right": 1
+  },
+
+
+  {
+    "title": 'O que é o webinar?',
+    "options": ["  Um produto barato oferecido depois do cadastro em uma lista de e - mails",
+      "Uma live para apresentar conteúdo e fazer ofertas",
+      "Publicidade online para quem já visitou o site do afiliado anteriormente",
+    ],
+    "right": 1
+  },
+]
