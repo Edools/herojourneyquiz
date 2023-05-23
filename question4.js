10b961670de28f6ae5b7359a289a7283afb748e6
Matheus Martins <matheusms280100@gmail.com>
2023-05-22 22:48:44 -0300

    add questions 4

---
+++ question4.js
@@ -0,0 +1,138 @@
+[
+  {
+    title: 'Quais das opções abaixo são plataformas ou gateways de pagamento? ',
+    options: [
+      'PagSeguro, PayPal e SparkPay',
+      'Hubspot, RD Station e ActiveCampaign',
+      'Trello, Mercado Livre e Omie',
+    ],
+    right: 0
+  },
+  {
+    title: 'Qual é o objetivo principal de uma estratégia de "lançamento semente" em marketing de infoprodutos?',
+    options: [
+      'Criar uma lista de e-mails qualificada antes do lançamento oficial do produto',
+      'Gerar vendas em massa logo após o lançamento oficial do produto',
+      'Gerar antecipação e validar o produto antes de seu lançamento oficial ',
+    ],
+    right: 2
+  },
+  {
+    title: 'Qual é a principal vantagem de usar um pixel de conversão em uma campanha de marketing de afiliados?',
+    options: [
+      'Rastreamento preciso das conversões',
+      'Maior alcance do público-alvo',
+      'Melhorar o SEO do site',
+    ],
+    right: 0
+  },
+  {
+    title: 'Qual é a diferença entre marketing de afiliados e marketing multinível?',
+    options: [
+      "Marketing de afiliados tem comissão por vendas; marketing multinível tem comissão por recrutamento",
+      "Marketing de afiliados tem comissão por recrutamento; marketing multinível tem comissão por vendas",
+      "Não há diferença entre os dois modelos de marketing",
+    ],
+    right: 0
+  },
+
+  {
+    title: 'Qual é a principal vantagem de criar a própria rede de afiliados ao invés de colocar seu infoproduto em um marketplace?',
+    options: [
+      'Acesso a uma ampla rede de afiliados em todo o mundo',
+      'Maior controle sobre a mensagem de marketing',
+      'Custos mais baixos de gerenciamento de afiliados',
+    ],
+    right: 1
+  },
+  {
+    title: 'Qual a diferença entre PLR e o mercado de afiliados?',
+    options: [
+      'PLR são direitos comerciais sobre um produto; ser afiliado é apenas promover.',
+      'PLR é apenas promover um produto; ser afiliado é ter direitos comerciais sobre ele.',
+      'Não há diferença entre os dois. ',
+    ],
+    right: 0
+  },
+  {
+    title: 'Qual das opções abaixo NÃO é uma forma de atrair potenciais clientes para a lista de e-mails do afiliado?',
+    options: [
+      'Webinars',
+      'Anúncios em jornais',
+      'Sorteios',
+    ],
+    right: 1
+  },
+  {
+    title: 'Qual o nome do termo em inglês para o processo de criar uma landing page otimizada para conversão?',
+    options: [
+      'A/B Testing',
+      'Split Testing',
+      'Conversion Rate Optimization',
+    ],
+    right: 2
+  },
+  {
+    title: 'Qual o nome do tipo de infoproduto em que o comprador paga uma assinatura mensal para ter acesso a conteúdos exclusivos?',
+    options: [
+      'Venda infinita',
+      'Lançamento perpétuo',
+      'Clube de assinatura',
+    ],
+    right: 2
+  },
+  {
+    title: 'Qual o nome do processo de obter tráfego para um site por meio de links de outros sites?',
+    options: [
+      'Link building',
+      'SEO on-page',
+      'SEO off-page',
+    ],
+    right: 0
+  },
+  {
+    title: 'O que significa a sigla KPI em marketing digital?',
+    options: [
+      'Key Performance Indicator',
+      'Keyword Performance Index',
+      'Keyword Position Insight',
+    ],
+    right: 0
+  },
+  {
+    title: 'Por que a mentoria é uma das melhores opções de primeiro produto?',
+    options: [
+      'Porque tem baixo custo de produção e a entrega pode ser ao vivo',
+      'Porque é um produto rico, que depende de aulas gravadas para funcionar',
+      'Porque é um tipo de produto que pode ser lançado sem construção de autoridade',
+    ],
+    right: 0
+  },
+  {
+    title: 'Preciso ter o infoproduto pronto para lançar?',
+    options: [
+      'Sim, porque é impossível vender na internet algo que ainda não está pronto.',
+      'Não, porque posso criar a oferta e nunca lançar o produto',
+      'Não, tem como fazer faixa antes de ter produto pronto, como no lançamento semente',
+    ],
+    right: 2
+  },
+  {
+    title: 'Qual é o tipo de lançamento que usa técnicas de escassez para alcançar uma grande audiência rapidamente?',
+    options: [
+      'Lançamento Interno',
+      'Lançamento Relâmpago',
+      'Lançamento Semente',
+    ],
+    right: 1
+  },
+  {
+    title: 'O que é um lançamento de desafio?',
+    options: [
+      'Um lançamento feito com um produto desafiador',
+      'Um lançamento feito com um preço muito baixo',
+      'Um lançamento feito com uma meta a ser alcançada pelos compradores',
+    ],
+    right: 2
+  },
+]
